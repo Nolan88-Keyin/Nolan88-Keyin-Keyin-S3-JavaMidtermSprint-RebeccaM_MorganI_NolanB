4c7bffb91c52bb9f117146db7ce50b8e238d10a0
rebecca3miller <rebecca.miller@keyin.com>
2026-06-23 17:08:01 -0230

    Medtrackingsystem class created and system collections initialized.

--- src/MedicationTrackingSystem.java
+++ src/MedicationTrackingSystem.java
@@ -1,3 +1,52 @@
+import java.util.ArrayList;
+
+/* MEDICATION TRACKING SYSTEM 
+ * This class manages the pharmacy system.
+ * Stores: Patients, doctors, medications, and prescriptions. 
+ */
+
 public class MedicationTrackingSystem {
-    
+
+    // List of all patients in the system
+    private ArrayList<Patient> patients;
+
+    // List of all doctors in the system
+    private ArrayList<Doctor> doctors;
+
+    // List of all medications available in the system
+    private ArrayList<Medication> medications;
+
+    // List of all prescriptions issued in the system
+    private ArrayList<Prescription> prescriptions;
+
+    /*  CONSTRUCTOR */
+    /*  creates empty list when the system starts */
+
+    public MedicationTrackingSystem () {
+        patients = new ArrayList<>();
+        doctors = new ArrayList<>();
+        medications = new ArrayList<>();
+        prescriptions = new ArrayList<>();
+    } 
+
+    /* adds a patient to the patients list */
+    public void addPatient (Patient patient) {
+        patients.add(patient);
+    }
+
+    /*  adds a doctor to the doctors list */
+    public void addDoctor (Doctor doctor) {
+        doctors.add(doctor);
+    }
+
+    /*  adds a medication to the medications array list */
+    public void addMedication (Medication medication) {
+        medications.add(medication);
+    }
+
+    /*  adds a prescriptions to the prescriptions array list */
+    public void addPrescription (Prescription prescription) {
+        prescriptions.add(prescription);
+    }
+
 }
